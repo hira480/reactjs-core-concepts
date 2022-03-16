716b59bf8d83fdd75a77d18d98c70daf5330f341
hira480 <hiramannan8@gmail.com>
2022-03-16 10:26:20 +0600

    component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 1234;
-const singer = { name: 'Atif Aslam', job: 'singer' };
-const singer2 = { name: 'Eva B', job: 'Rap Singer' };
+const singers = [
+  { name: 'Atif Aslam', job: 'singer' },
+  { name: 'Eva B', job: 'Rap Singer' },
+  { name: 'Rahat Fateh Ali', job: 'Gazal' },
+  { name: 'Momina', job: 'Beparwa' }
+];
 
 const singerStyle = {
   color: 'purple',
@@ -13,33 +17,46 @@ const singerStyle = {
 }
 
 function App() {
+  const nayoks = ['Rubel', 'Bapparaz', 'Kuber', 'Jashim', 'Salman Shah', 'Riyaj'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello Dude React. How are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222 + number}</p>
-          <p style={singerStyle}>Name: {singer.name} {singer.job}</p>
-          <p style={{ color: 'blue', backgroundColor: 'yellow' }}>Name: {singer2.name} {singer2.job}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>{nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayika="moushumi"></Person>
+      <Person name={nayoks[1]} nayika="cheka"></Person>
+      <Person name={nayoks[2]} nayika="kopila"></Person> */}
+      <h5>New Component</h5>
+      <Friend movie="Singham" phone="01777890654"></Friend>
+      <Friend movie="Dabang" phone='01999956789'></Friend>
     </div>
   );
+}
+
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  console.log(props)
+  return (
+    <div className="container">
+      <h3>Name: {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
